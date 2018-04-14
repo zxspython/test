433c3095a40790c512f90d52aabc78f07cd5d749
zxspython <4454230.hi@163.com>
2018-04-14 11:40:27 +0800

    Scrapy

---
+++ shiyanlou_courses_spider.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+import scrapy
+
+class ShiyanlouCoursesSpider(scrapy.Spider):
+    name = 'shiyanlou-courses'
+
+    @property
+    def start_urls(self):
+        url_tmpl = 'https://www.shiyanlou.com/courses/?category=all&course_type=all&fee=all&tag=all&page={}'
+        urls = (url_tmpl.format(i) for i in range(1,23))
+        return urls
+
+    def parse(self,response):
+        for course in response.css('div.course-body'):
+            yield{
+                    'name':course.css('div.course-name::text').extract_first(),
+                    'description':course.css('div.course-desc::text').extract_first(),
+                    'type':course.css('div.course-footer span.pull-right::text').extract_first(),
+                    'students':course.xpath('.//span[contains(@class,"pull-left")]/text()[2]').re_first('[^\d]*(\d+)[^\d]*')
+
+                    }
